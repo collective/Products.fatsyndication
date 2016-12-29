7ad4a2d8025c48823dd20d1ccfafb5621d3ea5a1
T. Kim Nguyen <tkimnguyen@users.noreply.github.com>
2016-12-29 12:55:07 -0600

    add classifier for Plone 4.3

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name='Products.fatsyndication',
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
         "Framework :: Zope2",
         "Framework :: Zope3",
         "Programming Language :: Python",
